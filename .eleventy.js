9e06e3b039f7ada5485f0c700f8d5af50a950c11
Isuru Nanayakkara <isuru.nanayakkara@itelasoft.com>
2022-12-31 15:41:24 +0530

    Created responsive page layout.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function (config) {
   // Input Directory : src Output directory: _site
+
+  config.addPassthroughCopy("src/img/");
+
   return {
     dir: { input: "src", output: "_site" },
   };
